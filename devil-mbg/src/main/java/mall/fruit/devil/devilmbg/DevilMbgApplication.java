f0484a1a836f272a7ad509e4f40179019ce7fb3b
zhangke <zhangke@justek.tv>
2022-05-18 17:56:08 +0800

    develop progress

---
+++ devil-mbg/src/main/java/mall/fruit/devil/devilmbg/DevilMbgApplication.java
@@ -0,0 +1,13 @@
+package mall.fruit.devil.devilmbg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevilMbgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevilMbgApplication.class, args);
+    }
+
+}
---
+++ devil-mbg/src/test/java/mall/fruit/devil/devilmbg/DevilMbgApplicationTests.java
@@ -0,0 +1,13 @@
+package mall.fruit.devil.devilmbg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevilMbgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
