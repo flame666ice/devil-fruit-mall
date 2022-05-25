abafdf9b6a28fd7e9f7e59c54c5dc58d39676294
zhangke <zhangke@justek.tv>
2022-05-25 18:03:59 +0800

    20220525

--- devil-mbg/src/main/java/mall/fruit/devil/devilmbg/controller/OmsCompanyAddressController.java
+++ devil-mbg/src/main/java/mall/fruit/devil/devilmbg/controller/OmsCompanyAddressController.java
@@ -1,7 +1,16 @@
 package mall.fruit.devil.devilmbg.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import mall.fruit.devil.devilcommon.api.CommonResult;
+import mall.fruit.devil.devilmbg.entity.OmsCompanyAddress;
+import mall.fruit.devil.devilmbg.service.impl.OmsCompanyAddressServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,7 +21,18 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2022-05-20
  */
 @RestController
-@RequestMapping("/omsCompanyAddress")
+@Api(tags = "OmsCompanyAddressController", description = "收货地址管理")
+@RequestMapping("/companyAddress")
 public class OmsCompanyAddressController {
+
+    @Autowired
+    private OmsCompanyAddressServiceImpl omsCompanyAddressService;
+
+    @ApiOperation("获取所有收货地址")
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public CommonResult<List<OmsCompanyAddress>> list(){
+        List<OmsCompanyAddress> omsCompanyAddressList = omsCompanyAddressService.list();
+        return CommonResult.success(omsCompanyAddressList);
+    }
 
 }
--- devil-mbg/src/main/java/mall/fruit/devil/devilmbg/controller/OmsOrderController.java
+++ devil-mbg/src/main/java/mall/fruit/devil/devilmbg/controller/OmsOrderController.java
@@ -1,7 +1,17 @@
 package mall.fruit.devil.devilmbg.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import mall.fruit.devil.devilcommon.api.CommonPage;
+import mall.fruit.devil.devilcommon.api.CommonResult;
+import mall.fruit.devil.devilmbg.entity.OmsOrder;
+import mall.fruit.devil.devilmbg.service.impl.OmsOrderServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.print.PrinterAbortException;
 
 /**
  * <p>
@@ -12,7 +22,18 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2022-05-20
  */
 @RestController
-@RequestMapping("/omsOrder")
+@Api(tags = "OmsOrderController", description = "订单管理")
+@RequestMapping("/order")
 public class OmsOrderController {
+
+    @Autowired
+    private OmsOrderServiceImpl omsOrderService;
+
+
+    @ApiOperation("查询订单")
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public CommonResult<CommonPage<OmsOrder>> list(OmsOrderQuery){
+
+    }
 
 }
