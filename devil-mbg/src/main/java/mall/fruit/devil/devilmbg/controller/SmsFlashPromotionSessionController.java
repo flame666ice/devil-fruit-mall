6761e3546625a16f0f6bf6b758a67a9429a2c7db
zhangke <zhangke@justek.tv>
2022-06-02 17:39:04 +0800

    20220602

--- devil-mbg/src/main/java/mall/fruit/devil/devilmbg/controller/SmsFlashPromotionSessionController.java
+++ devil-mbg/src/main/java/mall/fruit/devil/devilmbg/controller/SmsFlashPromotionSessionController.java
@@ -1,7 +1,23 @@
 package mall.fruit.devil.devilmbg.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import mall.fruit.devil.devilcommon.api.CommonResult;
+import mall.fruit.devil.devilmbg.entity.SmsFlashPromotionProductRelation;
+import mall.fruit.devil.devilmbg.entity.SmsFlashPromotionSession;
+import mall.fruit.devil.devilmbg.entitycustom.SmsFlashPromotionSessionDetail;
+import mall.fruit.devil.devilmbg.mapper.SmsFlashPromotionProductRelationMapper;
+import mall.fruit.devil.devilmbg.mapper.SmsFlashPromotionSessionMapper;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * <p>
@@ -12,7 +28,92 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2022-05-20
  */
 @RestController
-@RequestMapping("/smsFlashPromotionSession")
+@Api(tags = "SmsFlashPromotionSessionController", description = "限时购场次管理")
+@RequestMapping("/flashSession")
 public class SmsFlashPromotionSessionController {
 
+
+    @Autowired
+    private SmsFlashPromotionSessionMapper promotionSessionMapper;
+
+    @Autowired
+    private SmsFlashPromotionProductRelationMapper relationMapper;
+
+    @ApiOperation("添加场次")
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public CommonResult create(SmsFlashPromotionSession promotionSession) {
+        promotionSession.setCreateTime(LocalDateTime.now());
+        int count = promotionSessionMapper.insert(promotionSession);
+        if (count > 0) {
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("修改场次")
+    @RequestMapping(value = "/update/{id}", method = RequestMethod.POST)
+    public CommonResult update(@PathVariable Long id,  SmsFlashPromotionSession promotionSession) {
+        promotionSession.setId(id);
+        int count = promotionSessionMapper.updateById(promotionSession);
+        if (count > 0) {
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("修改启用状态")
+    @RequestMapping(value = "/update/status/{id}", method = RequestMethod.POST)
+    public CommonResult updateStatus(@PathVariable Long id, Integer status) {
+        SmsFlashPromotionSession promotionSession = new SmsFlashPromotionSession();
+        promotionSession.setId(id);
+        promotionSession.setStatus(status);
+        int count = promotionSessionMapper.updateById(promotionSession);
+        if (count > 0) {
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("删除场次")
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.POST)
+    public CommonResult delete(@PathVariable Long id) {
+        int count = promotionSessionMapper.deleteById(id);
+        if (count > 0) {
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("获取场次详情")
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public CommonResult<SmsFlashPromotionSession> getItem(@PathVariable Long id) {
+        SmsFlashPromotionSession promotionSession = promotionSessionMapper.selectById(id);
+        return CommonResult.success(promotionSession);
+    }
+
+    @ApiOperation("获取全部场次")
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public CommonResult<List<SmsFlashPromotionSession>> list() {
+        List<SmsFlashPromotionSession> promotionSessionList = promotionSessionMapper.selectList(Wrappers.emptyWrapper());
+        return CommonResult.success(promotionSessionList);
+    }
+
+    @ApiOperation("获取全部可选场次及其数量")
+    @RequestMapping(value = "/selectList", method = RequestMethod.GET)
+    public CommonResult<List<SmsFlashPromotionSessionDetail>> selectList(Long flashPromotionId) {
+        List<SmsFlashPromotionSessionDetail> result = new ArrayList<>();
+        QueryWrapper<SmsFlashPromotionSession> wrapper = new QueryWrapper<>();
+        wrapper.eq("status",1);
+        List<SmsFlashPromotionSession> list = promotionSessionMapper.selectList(wrapper);
+        for (SmsFlashPromotionSession promotionSession : list) {
+            SmsFlashPromotionSessionDetail detail = new SmsFlashPromotionSessionDetail();
+            BeanUtils.copyProperties(promotionSession, detail);
+            QueryWrapper<SmsFlashPromotionProductRelation> wrapper1 = new QueryWrapper<>();
+            wrapper1.eq("flash_promotion_id",flashPromotionId).eq("flash_promotion_session_id",promotionSession.getId());
+            long count = relationMapper.selectCount(wrapper1);
+            detail.setProductCount(count);
+            result.add(detail);
+        }
+        return CommonResult.success(result);
+    }
 }
--- devil-mbg/src/main/java/mall/fruit/devil/devilmbg/controller/SmsHomeNewProductController.java
+++ devil-mbg/src/main/java/mall/fruit/devil/devilmbg/controller/SmsHomeNewProductController.java
@@ -1,7 +1,20 @@
 package mall.fruit.devil.devilmbg.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.sun.deploy.net.proxy.RemoveCommentReader;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import mall.fruit.devil.devilcommon.api.CommonPage;
+import mall.fruit.devil.devilcommon.api.CommonResult;
+import mall.fruit.devil.devilmbg.entity.SmsHomeNewProduct;
+import mall.fruit.devil.devilmbg.mapper.SmsHomeNewProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,7 +25,84 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2022-05-20
  */
 @RestController
-@RequestMapping("/smsHomeNewProduct")
+@Api(tags = "SmsHomeNewProductController", description = "首页新品管理")
+@RequestMapping("/home/newProduct")
 public class SmsHomeNewProductController {
+
+    @Autowired
+    private SmsHomeNewProductMapper homeNewProductMapper;
+
+    @ApiOperation("添加首页新品")
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public CommonResult create( List<SmsHomeNewProduct> homeNewProductList) {
+        for (SmsHomeNewProduct SmsHomeNewProduct : homeNewProductList) {
+            SmsHomeNewProduct.setRecommendStatus(1);
+            SmsHomeNewProduct.setSort(0);
+            homeNewProductMapper.insert(SmsHomeNewProduct);
+        }
+        int count =  homeNewProductList.size();
+        if (count > 0) {
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("修改首页新品排序")
+    @RequestMapping(value = "/update/sort/{id}", method = RequestMethod.POST)
+    public CommonResult updateSort(@PathVariable Long id, Integer sort) {
+        SmsHomeNewProduct homeNewProduct = new SmsHomeNewProduct();
+        homeNewProduct.setId(id);
+        homeNewProduct.setSort(sort);
+        int count = homeNewProductMapper.updateById(homeNewProduct);
+        if (count > 0) {
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("批量删除首页新品")
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    public CommonResult delete(@RequestParam("ids") List<Long> ids) {
+        QueryWrapper<SmsHomeNewProduct> wrapper = new QueryWrapper<>();
+        wrapper.in("id",ids);
+        int count = homeNewProductMapper.delete(wrapper);
+        if (count > 0) {
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("批量修改首页新品状态")
+    @RequestMapping(value = "/update/recommendStatus", method = RequestMethod.POST)
+    public CommonResult updateRecommendStatus(@RequestParam("ids") List<Long> ids, @RequestParam Integer recommendStatus) {
+        UpdateWrapper<SmsHomeNewProduct> wrapper = new UpdateWrapper<>();
+        wrapper.in("id",ids);
+        SmsHomeNewProduct record = new SmsHomeNewProduct();
+        record.setRecommendStatus(recommendStatus);
+        int count = homeNewProductMapper.update(record,wrapper);
+        if (count > 0) {
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("分页查询首页新品")
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public CommonResult<CommonPage<SmsHomeNewProduct>> list(@RequestParam(value = "productName", required = false) String productName,
+                                                            @RequestParam(value = "recommendStatus", required = false) Integer recommendStatus,
+                                                            @RequestParam(value = "pageSize", defaultValue = "5") Integer pageSize,
+                                                            @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum) {
+        Page<SmsHomeNewProduct> page = new Page<>(pageNum,pageSize);
+        QueryWrapper<SmsHomeNewProduct> wrapper = new QueryWrapper<>();
+        if(!StringUtils.isEmpty(productName)){
+            wrapper.like("product_name",productName);
+        }
+        if(recommendStatus!=null){
+            wrapper.eq("recommend_status", recommendStatus);
+        }
+        wrapper.orderByDesc("sort");
+        Page<SmsHomeNewProduct> result = homeNewProductMapper.selectPage(page,wrapper);
+        return CommonResult.success(CommonPage.restPage(result));
+    }
 
 }
